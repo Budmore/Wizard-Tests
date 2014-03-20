f21ab9c0f1c6858782653bf1700ea3ee31027077
Technik- <jmach@neoteric.eu>
2014-03-20 23:52:46 +0100

    Mock model and active step

--- app/modules/navrules-program/controllers/StepController.js
+++ app/modules/navrules-program/controllers/StepController.js
@@ -1,7 +1,57 @@
-angular.module('navrules-program').controller('StepController', function() {
-	'use strict';
+angular.module('navrules-program').controller('StepController', ['$scope', 'modelHelper', '$exceptionHandler',
+	function($scope, modelHelper, $exceptionHandler) {
+		'use strict';
+		var self=this;
+
+		this.getSteps = function() {
+			modelHelper.getSteps().then(function() {
+				$scope.steps = modelHelper.getModel();
+				$scope.stepNr = 1;
+				$scope.nextStep();
+			}, function(reason) {
+				$exceptionHandler(reason);
+			});
+		};
+
+		this.activeStep = function(nrStep) {
+			$scope.step = $scope.steps[nrStep - 1];
+			console.log($scope.step);
+
+			$scope.imageUrl = $scope.step.imageUrl.question;
+			$scope.showOutline = false;
+			$scope.showNavRule = false;
+		};
+
+		$scope.nextStep = function() {
+			console.log('nextStep', $scope.stepNr, $scope.steps.length);
+			self.activeStep($scope.stepNr);
+
+			if ($scope.stepNr < $scope.steps.length) {
+				$scope.stepNr += 1;
+			}
+		};
 
 
-});
+		$scope.changeImage = function() {
+
+			if ($scope.showOutline) {
+				$scope.imageUrl = $scope.step.imageUrl.question;
+			} else {
+				$scope.imageUrl = $scope.step.imageUrl.outline;
+			}
+			$scope.showOutline = !$scope.showOutline;
+		};
+
+		$scope.showNavigationRule = function() {
+			console.log('clicked');
+			$scope.showRule = !$scope.showRule;
+		};
+
+		(function() {
+			self.getSteps();
+		})();
+
+	}
+]);
 
 
--- app/modules/settings.js
+++ app/modules/settings.js
@@ -6,12 +6,5 @@
 // Environment
 var DEBUG = false;
 
-// Current version
-version = {
-	iteration: 2,
-	backend: 1,
-	frontend: 1
-};
-
 // Setup locales - language
 var t = { pl: {}, en: {}};
