a2a2e6b3a543d8970796bbb0ca7e4eb906549b82
Technik- <jmach@neoteric.eu>
2014-03-26 23:37:14 +0100

    urls

--- app/modules/urls.js
+++ app/modules/urls.js
@@ -1,11 +1,9 @@
 angular.module('urls', ['ngRoute']).config(['$routeProvider',
 	function($routeProvider) {
 		$routeProvider.
-			// when('/', {templateUrl: 'app/modules/navrules-program/views/index.html' }).
-			when('/step', { templateUrl: 'app/modules/navrules-program/views/step.html'	}).
 
 			otherwise({
-				redirectTo: '/'
+				redirectTo: '/step'
 			});
 	}
 ]);
