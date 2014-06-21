871dd05dce6018df6aa7fd88d492a69c1c196b9f
Technik- <jmach@neoteric.eu>
2014-06-21 18:05:03 +0200

    remove unused files (v1)

--- app/modules/navrules-program/urls.js
+++
@@ -1,9 +0,0 @@
-angular.module('navrules-program').config(['$routeProvider',
-	function($routeProvider) {
-		$routeProvider.
-			// when('/', {templateUrl: 'app/modules/navrules-program/views/index.html' }).
-			when('/step', { templateUrl: 'app/modules/navrules-program/views/step.html'	}).
-			when('/result', { templateUrl: 'app/modules/navrules-program/views/result.html'});
-	}
-]);
-
